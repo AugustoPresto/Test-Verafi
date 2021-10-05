c60aac28cca1116a64c3b7361b6a8d2efbff9097
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-10-05 10:43:03 -0300

    created server_status strong params and added skip authentication to show method

--- app/controllers/server_statuses_controller.rb
+++ app/controllers/server_statuses_controller.rb
@@ -1,2 +1,17 @@
 class ServerStatusesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :show ]
+
+  def show
+
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def server_status_params
+    params.require(:article).permit(:status, :message)
+  end
 end
